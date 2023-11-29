2e8993d73be6c17c688c1bcf432c6c3fa59bb934
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2023-11-29 15:41:38 +0200

    minor changes

--- Timeflow.Platform.Api/Boundary/Response/TaskResponse.cs
+++ Timeflow.Platform.Api/Boundary/Response/TaskResponse.cs
@@ -8,10 +8,19 @@ namespace Timeflow.Platform.Api.Boundary.Response
         [JsonProperty("Id")]
         public int Id { get; set; }
 
+        [JsonProperty("ProjectId")]
+        public int ProjectId { get; set; }
+
+        [JsonProperty("Title")]
+        public string Title { get; set; } = null!;
+
         [JsonProperty("ServiceTypeId")]
         public ServiceTypeEnum ServiceTypeId { get; set; }
 
         [JsonProperty("ServiceType")]
         public string ServiceType { get; set; } = null!;
+
+        [JsonProperty("Description")]
+        public string Description { get; set; } = null!;
     }
 }
